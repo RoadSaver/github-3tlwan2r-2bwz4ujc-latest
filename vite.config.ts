1e12f94030f978816ee46da9ea1eb337c585facc
RoadSaver <roadsaverapp@gmail.com>
2025-06-07 23:47:14 +0300

    Fix Vite React plugin configuration

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(({ mode }) => ({
     host: '0.0.0.0',
     port: 8080,
   },
-  plugins: [react(), printExternalLink()],
+  plugins: [react({ include: '**/*.tsx' }), printExternalLink()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
